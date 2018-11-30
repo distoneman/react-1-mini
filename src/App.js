cbd9a220de38ec0d4a933174aad97865e37e2f3f
distoneman <distoneman@gmail.com>
2018-11-30 15:09:06 -0700

    app.js

--- src/App.js
+++ src/App.js
@@ -12,29 +12,56 @@ class App extends Component {
     }
   }
 
-  updatePicture(value){
+  updatePicture(value) {
     this.setState({
       picture: value
     })
   }
 
-  updateName(value){
+  updateName(value) {
     this.setState({
       name: value
     })
   }
 
+  addFriend() {
+    let { friends } = this.state;
+
+    let newFriends = friends.slice();
+    newFriends.push({ name: this.state.name, picture: this.state.picture });
+
+    this.setState({
+      friends: newFriends,
+      picture: '',
+      name: ''
+    });
+  }
+
   render() {
+    const { friends } = this.state
+    let displayFriends = friends.map((friend, index) => {
+      console.log(friend, index)
+      return (
+        <div key={index}>
+          <img src={friend.picture} alt='friend'/>
+          <span>
+            {friend.name}
+          </span>
+        </div>
+      )
+    })
+    console.log(displayFriends)
     return (
       <div>
-        <label>URL to picture</label>
+        <label>URL to Picture</label>
         <input onChange={(e) => this.updatePicture(e.target.value)}
-          value = {this.state.picture}/>
+          value={this.state.picture} />
         <label>Friends Name</label>
-        <input onChange = {(e) => this.updateName(e.target.value)} 
-          value = {this.state.name} />
-        <button>Add Friend</button>
-      </div>
+        <input onChange={(e) => this.updateName(e.target.value)}
+          value={this.state.name} />
+        <button onClick={() => this.addFriend()}>Add Friend</button>
+        {displayFriends}
+      </div >
     );
   }
 }
